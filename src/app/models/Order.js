08c52c295f332c8033836d6c79be24a9a66087ef
Nguyenle23___ <beenbin2308@gmail.com>
2022-04-16 21:18:23 +0700

    Update Order.js

--- src/app/models/Order.js
+++ src/app/models/Order.js
@@ -3,10 +3,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let Order = new Schema({
-    userId: { type: Schema.Types.ObjectId, ref: 'Users' },
-    orderStatus: { type: String, maxLength: 255 },
-    orderAmount: { type: Number, default: 0 },
-    orderTracker: { type: String, maxLength: 255 },
+    // userId: { type: Schema.Types.ObjectId, ref: 'Users' },
+    // orderStatus: { type: String, maxLength: 255 },
+    // orderAmount: { type: Number, default: 0 },
+    // orderTracker: { type: String, maxLength: 255 },
+    name: { type: String, maxLength: 255 },
+    phonenumber: { type: String, maxLength: 255 },
+    address: { type: String, maxLength: 255 },
+    note: { type: String, maxLength: 255 },
     items: [{
         sku: [{ type: String }],
         qty: [{ type: String }],
